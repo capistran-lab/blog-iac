b49ed30dadfb3106473a2b1e06aa08442e85e033
Ulises <ucapistranmtz@gmail.com>
2026-02-17 14:26:34 -0600

    fix s3 id

--- lambda.tf
+++ lambda.tf
@@ -1,40 +1,21 @@
-# Este es el bloque que te falta declarar
-data "archive_file" "auth_placeholder" {
-  type        = "zip"
-  output_path = "${path.module}/dummy_auth.zip"
-
-  source {
-    content  = "exports.handler= async()=> {return {statusCode:200, body:'Auth Placeholder'}};"
-    filename = "index.js"
-  }
-}
-resource "aws_s3_object" "auth_placeholder_upload" {
-  bucket = aws_s3_bucket.artifacts_storage.id
-  key    = "auth-handler.zip"
-  source = data.archive_file.auth_placeholder.output_path
-
-  # Solo lo sube si no existe; luego GitHub Actions se encarga
-  lifecycle {
-    ignore_changes = [source, etag]
-  }
-}
-
 resource "aws_lambda_function" "auth_handler" {
   function_name = "${var.project_name}-auth-handler"
   role          = aws_iam_role.auth_lambda_role.arn
   handler       = "index.handler"
   runtime       = "nodejs20.x"
   memory_size   = 512
-  s3_bucket     = "blog-website-artifacts"
-  s3_key        = "auth-handler.zip"
-  # Quita el source_code_hash para que Terraform no se pelee con el Action
+
+  s3_bucket = "blog-website-artifacts"
+  s3_key    = "auth-handler.zip"
+
+  s3_object_version = aws_s3_object.auth_placeholder_upload.version_id
+
+  depends_on = [aws_s3_object.auth_placeholder_upload]
 
   lifecycle {
     ignore_changes = [
       s3_key,
       source_code_hash,
-      last_modified,
-      # Esto es clave:
       s3_object_version
     ]
   }
